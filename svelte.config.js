6677ace5720a014f65ff4f0c6bd9bfa00cac1c6f
Fernando Pimentel <fernandopimenton@gmail.com>
2022-11-19 16:50:28 -0600

    Initial commit

--- svelte.config.js
+++ svelte.config.js
@@ -1,8 +1,9 @@
-
+import preprocess from 'svelte-preprocess';
 import adapter from '@sveltejs/adapter-static';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
+	preprocess: preprocess(),
 	kit: {
 		adapter: adapter()
 	}
